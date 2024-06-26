0fe5def9bf333ddcf61d6394b637576ce646c08b
Mobin Akhtar <Megamobin@gmail.com>
2024-06-26 11:38:04 -0400

    Cambio en modelo de db y agregados los modelos respecto al cambio de db

--- mysite/gestion/models.py
+++ mysite/gestion/models.py
@@ -1,106 +1,142 @@
 from django.db import models
 
 # Modelos
-class Secretario(models.Model):
-    ci = models.AutoField(primary_key=True)
-    first_name = models.CharField(max_length=50, default="secretario")
-    last_name = models.CharField(max_length=50, default="apellido_secretario_opcional")
-    birth_date = models.DateField(default="2024-07-01")
-    email = models.EmailField(default="email")
-    phone = models.CharField(max_length=50, default="0")
-    address = models.CharField(max_length=50, default="desconocido")
+
+class Person(models.Model):
+    id_person = models.IntegerField(primary_key=True)       #ci de la persona
+    first_name = models.CharField(max_length=50)
+    last_name = models.CharField(max_length=50)
+    birth_date = models.DateField(auto_now_add=True)
     class Meta:
-        db_table = 'secretario'
-        ordering = ['last_name', 'first_name']
-        verbose_name = 'Secretario'
-        verbose_name_plural = 'Secretarios'
-    def __str__(self):
-        return f"{self.first_name} {self.last_name}"
+        db_table = 'person'
+        verbose_name_plural = 'Personas'
+        verbose_name = 'Persona'
+        ordering = ['first_name', 'last_name']
+    def datosPersona(self):
+        return self.id_person, self.first_name, self.last_name, self.birth_date
+
+class User(models.Model):
+    id_user = models.AutoField(primary_key=True)            #identificador unico, definido por el sistema
+    id_person = models.ForeignKey(Person, on_delete=models.CASCADE)
+    email = models.EmailField(max_length=50, unique=True)
+    pass_hash = models.CharField(max_length=50)
+    salt = models.CharField(max_length=50)
+    tipe = [
+        ('EST', 'Estudiante'),
+        ('PRO', 'Profesor'),
+        ('ADM', 'Administrativo'),
+    ]
+    user_tipe = models.CharField(max_length=3, choices=tipe, default='EST')
+    if user_tipe == 'EST':
+        carrer = models.ForeignKey(Carrer, null=False, blank=False, on_delete=models.CASCADE)
+    class Meta:
+        db_table = 'user'
+        verbose_name_plural = 'Usuarios'
+        verbose_name = 'Usuario'
+        ordering = ['id_user', 'id_person']
+    def datosUser(self):
+        return self.id_user, self.email, self.user_tipe
 
 class Student(models.Model):
-    ci = models.AutoField(primary_key=True, auto_created=True)
-    first_name = models.CharField(max_length=50, default="desconocido")
-    last_name = models.CharField(max_length=50, default="desconocido")
-    birth_date = models.DateField(default="2024-07-01")
-    email = models.EmailField(default="email")
-    phone = models.CharField(max_length=50, default="0")
-    address = models.CharField(max_length=50, default="desconocido")
-    enrollment_date = models.DateField(default='2024-07-01')
+    id_student = models.IntegerField(primary_key=True)      #identificador unico definido por el sistema
+    id_person = models.ForeignKey(Person, on_delete=models.CASCADE)
+    id_carrer = models.ForeignKey(Carrer, null=False, blank=False, on_delete=models.CASCADE)
+    status = models.BooleanField(default=True)
     class Meta:
-        db_table = 'estudiante'
+        db_table = 'student'
         verbose_name_plural = 'Estudiantes'
         verbose_name = 'Estudiante'
-        ordering = ['first_name', 'last_name']
-        permissions = [
-            ("can_view_grades", "Puede ver sus calificaciones"),
-            ("can_edit_profile", "Puede editar su perfil"),
-        ]
-    def __str__(self):
-        return f"{self.first_name} {self.last_name}"
+        ordering = ['id_student', 'id_person']
+    def nombreCompleto(self):
+        txt = "{0} {1}, {2}"
+        return txt.format(self.id_student, self.id_person, self.id_carrer)
 
-class Profesor(models.Model):
-    ci = models.AutoField(primary_key=True, auto_created=True)
-    first_name = models.CharField(max_length=50, default="desconocido")
-    last_name = models.CharField(max_length=50, default="desconocido")
-    hire_date = models.DateField(default='2024-07-01')
-    specialization = models.CharField(max_length=100, default="desconocido")
+class Carrer(models.Model):
+    id_carrer = models.AutoField(primary_key=True)
+    name_carrer = models.CharField(max_length=50)
+    faculty = models.CharField(max_length=50)
+    class Meta:
+        db_table = 'carrer'
+        verbose_name_plural = 'Carreras'
+        verbose_name = 'Carrera'
+        ordering = ['id_carrer', 'name_carrer']
+    def datosCarrer(self):
+        return self.name_carrer, self.faculty
+
+class Teacher(models.Model):
+    id_teacher = models.IntegerField(primary_key=True)
+    id_person = models.ForeignKey(Person, on_delete=models.CASCADE)
+    specialization = models.CharField(max_length=50)
     class Meta:
         db_table = 'profesor'
         verbose_name_plural = 'Profesores'
         verbose_name = 'Profesor'
-        ordering = ['first_name', 'last_name']
-        permissions = [
-            ("can_edit_grades", "Puede editar calificaciones"),
-            ("can_edit_profile", "Puede editar su perfil"),
-            ("can_view_profile", "Puede ver su perfil"),
-            ("can_view_grades", "Puede ver las calificaciones de los estudiantes"),
-        ]
-    def __str__(self):
-        return f"{self.first_name} {self.last_name} - {self.specialization}"
+        ordering = ['id_teacher', 'id_person']
+    def datosProfesor(self):
+        return self.id_teacher, self.id_person, self.specialization
 
-class Curso(models.Model):
-    id = models.AutoField(primary_key=True, auto_created=True)
-    name = models.CharField(max_length=50, default="unknown")
-    description = models.TextField(default="vacio")
-    teacher = models.ForeignKey(Profesor, on_delete=models.CASCADE, null=True, blank=True)
-
+class Course(models.Model):
+    id_course = models.AutoField(primary_key=True)
+    course_name = models.CharField(max_length=50)
+    course_description = models.TextField()
+    id_carrer = models.ForeignKey(Carrer, on_delete=models.CASCADE)
+    id_subject = models.ForeignKey(Subject, on_delete=models.CASCADE)
     class Meta:
+        db_table = 'course'
         verbose_name_plural= 'Cursos'
         verbose_name = 'Curso'
-        ordering = ['name']
-    def __str__(self):
-        return self.name
+        ordering = ['id_course', 'course_name']
+    def datosCourse(self):
+        return self.id_course, self.course_name, self.course_description, self.id_carrer, self.id_subject
 
-class Matricula(models.Model):
-    id = models.AutoField(primary_key=True, auto_created=True)
-    student = models.ForeignKey(Student, on_delete=models.CASCADE, null=True)
-    course = models.ForeignKey(Curso, on_delete=models.CASCADE, null=True, blank=True)
-    enrollment_date = models.DateField(default='2024-07-01')
+class Registration(models.Model):
+    id_registration = models.AutoField(primary_key=True, auto_created=True)
+    id_student = models.ForeignKey(Student, on_delete=models.CASCADE)
+    id_subject = models.ForeignKey(Carrer, on_delete=models.CASCADE)
+    registration_date = models.DateField(auto_now_add=True)
     class Meta:
+        db_table = 'registration'
         verbose_name_plural = 'Matriculas'
         verbose_name = 'Matricula'
-        ordering = ['student', 'course']
-    def __str__(self):
-        return f"{self.student} enrolled in {self.course} on {self.enrollment_date}"
+        ordering = ['id_registration', 'id_student', 'id_subject']
+    def datosRegistration(self):
+        return self.id_registration, self.id_student, self.id_subject, self.registration_date
 
-class Asignatura(models.Model):
-    id = models.AutoField(primary_key=True, auto_created=True)
-    name = models.CharField(max_length=100, default="unknow")
-    course = models.ForeignKey(Curso, related_name='subjects', on_delete=models.CASCADE, null=True)
+class Subject(models.Model):
+    id_subject = models.AutoField(primary_key=True, auto_created=True)
+    id_teacher = models.ForeignKey(Teacher, on_delete=models.CASCADE)
+    id_academicPeriod = models.ForeignKey(AcademicPeriod, on_delete=models.CASCADE)
     class Meta:
+        db_table = 'subject'
         verbose_name_plural = 'Asignaturas'
         verbose_name = 'Asignatura'
-        ordering = ['name']
-    def __str__(self):
-        return self.name
+        ordering = ['id_subject', 'id_teacher']
+    def datosAsignatura(self):
+        return self.id_subject, self.id_teacher, self.id_academicPeriod
 
-class Grade(models.Model):
-    student = models.ForeignKey(Student, on_delete=models.CASCADE, related_name='grade', default=None, null=True, blank=True)
-    subject = models.CharField(max_length=100, verbose_name="Asignatura", default='asignatura')
-    grade = models.DecimalField(max_digits=5, decimal_places=2, verbose_name="Calificación", default=0)
-    date_recorded = models.DateField(verbose_name="Fecha de Registro", default='2024-07-01')
+class AcademicPeriod(models.Model):
+    id_academicPeriod = models.AutoField(primary_key=True, auto_created=True)
+    name_academicPeriod = models.CharField(max_length=50)
+    year_academicPeriod = models.DateField(auto_now_add=True)
+    semester_academicPeriod = models.IntegerField()
     class Meta:
+        db_table = 'academicperiod'
+        verbose_name_plural = 'Periodos Academicos'
+        verbose_name = 'Periodo Academico'
+        ordering = ['id_academicPeriod', 'year_academicPeriod']
+    def datosAacademicPeriod(self):
+        return self.id_academicPeriod, self.name_academicPeriod, self.year_academicPeriod, self.semester_academicPeriod
+
+class Grades(models.Model):
+    id_grades = models.AutoField(primary_key=True, auto_created=True)
+    id_student = models.ForeignKey(Student, on_delete=models.CASCADE)
+    id_curse = models.ForeignKey(Curse, on_delete=models.CASCADE)
+    id_subject = models.ForeignKey(Subject, on_delete=models.CASCADE)
+    note = models.TextField(default='')
+    evaluation_date = models.DateField(auto_now_add=True)
+    class Meta:
+        db_table = 'grades'
         verbose_name = 'Calificación'
         verbose_name_plural = 'Calificaciones'
-    def __str__(self):
-        return f"{self.student} - {self.subject}: {self.grade}"
+    def datosGrades(self):
+        return self.id_subject, self.id_student, self.id_curse, self.id_subject, self.note, self.evaluation_date
