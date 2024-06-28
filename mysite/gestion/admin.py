0ee5f67e6c295a31a5b40f96a3f802aadb20d200
Mobin Akhtar <Megamobin@gmail.com>
2024-06-28 15:31:02 -0400

    agregado opcion de asignar materia a teacher y horario a materia(dia y hora). y cambios reflejados en la db

--- mysite/gestion/admin.py
+++ mysite/gestion/admin.py
@@ -7,10 +7,10 @@ from .models import *
 class PersonAdmin(admin.ModelAdmin):
     list_display = ('id_person', 'first_name', 'last_name', 'birth_date')
     search_fields = ['first_name', 'last_name']
-@admin.register(Carrer)
-class CarrerAdmin(admin.ModelAdmin):
-    list_display = ('id_carrer', 'name_carrer', 'faculty')
-    search_fields = ['name_carrer', 'faculty']
+@admin.register(Career)
+class CareerAdmin(admin.ModelAdmin):
+    list_display = ('id_career', 'name_career', 'faculty')
+    search_fields = ['name_career', 'faculty']
 @admin.register(User)
 class UserAdmin(admin.ModelAdmin):
     list_display = ('id_user', 'id_person', 'email', 'user_type')
@@ -18,12 +18,12 @@ class UserAdmin(admin.ModelAdmin):
     search_fields = ['email', 'id_person__first_name', 'id_person__last_name']
 @admin.register(Student)
 class StudentAdmin(admin.ModelAdmin):
-    list_display = ('id_student', 'id_person', 'id_carrer', 'status')
-    list_filter = ['status', 'id_carrer']
+    list_display = ('id_student', 'id_person', 'id_career', 'status')
+    list_filter = ['status', 'id_career']
     search_fields = ['id_person__first_name', 'id_person__last_name']
 @admin.register(Teacher)
 class TeacherAdmin(admin.ModelAdmin):
-    list_display = ('id_teacher', 'id_person', 'specialization')
+    list_display = ('id_teacher', 'id_person', 'id_subject')
     search_fields = ['id_person__first_name', 'id_person__last_name', 'specialization']
 @admin.register(AcademicPeriod)
 class AcademicPeriodAdmin(admin.ModelAdmin):
@@ -32,13 +32,13 @@ class AcademicPeriodAdmin(admin.ModelAdmin):
     search_fields = ['name_academicPeriod']
 @admin.register(Subject)
 class SubjectAdmin(admin.ModelAdmin):
-    list_display = ('id_subject', 'id_academicPeriod')
+    list_display = ('id_subject', 'name_subject', 'id_academicPeriod')
     list_filter = ['id_academicPeriod']
     search_fields = ['id_teacher__id_person__first_name', 'id_teacher__id_person__last_name']
 @admin.register(Course)
 class CourseAdmin(admin.ModelAdmin):
-    list_display = ('id_course', 'course_name', 'id_carrer', 'id_subject')
-    list_filter = ['id_carrer']
+    list_display = ('id_course', 'course_name', 'id_career', 'id_subject')
+    list_filter = ['id_career']
     search_fields = ['course_name', 'course_description']
 @admin.register(Registration)
 class RegistrationAdmin(admin.ModelAdmin):
