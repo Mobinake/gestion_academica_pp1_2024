f1fd3907a9fec37eca594fe4bd29fdc2320ef26b
Mobin Akhtar <Megamobin@gmail.com>
2024-12-18 14:13:21 -0300

    Reformateo

--- mysite/gestion/tests.py
+++ mysite/gestion/tests.py
@@ -1,2 +1,3 @@
 # TODO: hacer tests para saber cuantar personas pueden usar el sistema sin que caiga y para verificar que todos los views/urls/modelos funcionan correctamente.
 # listar materias, usuarios, detalles, consultas a modelos
+
